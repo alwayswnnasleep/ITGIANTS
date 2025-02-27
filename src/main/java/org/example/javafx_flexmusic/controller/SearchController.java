c977883c6ecfcf75e4ef71e3b70eaba2cd114a2c
Pavel <alwayswnnasleep@gmail.com>
2025-02-27 09:56:06 +0300

    1

--- src/main/java/org/example/javafx_flexmusic/controller/SearchController.java
+++ src/main/java/org/example/javafx_flexmusic/controller/SearchController.java
@@ -41,50 +41,13 @@ public class SearchController implements Initializable {
             column_duration.setCellValueFactory(new PropertyValueFactory<>("duration"));
 
             table_tracks.setItems(trackList);
-            loadTracks();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-//    private void loadTracks() {
-//        List<Track> hardcodedTracks = Arrays.asList(
-//                new Track(1, 1, "3:30", "Song 1", "http://example.com/song1", "Genre 1", "Artist 1"),
-//                new Track(2, 1, "4:15", "Song 2", "http://example.com/song2", "Genre 2", "Artist 2"),
-//                new Track(3, 1, "2:45", "Song 3", "http://example.com/song3", "Genre 3", "Artist 3")
-//        );
-//
-//        // Добавляем треки в список
-//        trackList.clear();
-//        trackList.addAll(hardcodedTracks);
-//    }
 
 
-    private void loadTracks() {
-        Task<List<Track>> task = new Task<List<Track>>() {
-            @Override
-            protected List<Track> call() throws Exception {
-                Client client = new Client();
-                return client.getAllTracks();
-            }
 
-            @Override
-            protected void succeeded() {
-                trackList.clear();
-                trackList.addAll(getValue());
-            }
-
-            @Override
-            protected void failed() {
-                Throwable exception = getException();
-                exception.printStackTrace();
-            }
-        };
-        new Thread(task).start();
-    }
-
-    @FXML
-    private void handleRefreshIconClick() {
-        loadTracks();
-    }
 }
